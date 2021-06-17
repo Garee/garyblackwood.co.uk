384269d67eb3c8f228b44b7384bcfd8384f573bc
Gary Blackwood <gary@garyblackwood.co.uk>
2021-06-17 10:56:31 +0100

    Add a light/dark theme toggle

--- src/components/partials/footer/footer.tsx
+++ src/components/partials/footer/footer.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
+import Moon from "../../../images/theme/moon.inline.svg";
+import Sun from "../../../images/theme/sun.inline.svg";
+
 import * as styles from "./footer.module.css";
+
+const ThemeToggle = () => {
+    const [dark, setDark] = useState(prefersDarkTheme());
+
+    if (dark) {
+        document.body.classList.add("dark");
+    }
+
+    function prefersDarkTheme() {
+        const preference = window.localStorage.getItem("darkTheme");
+        if (preference !== null) {
+            return preference === "true";
+        }
+
+        return (
+            window.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark").matches
+        );
+    }
+
+    function toggleTheme() {
+        if (dark) {
+            document.body.classList.remove("dark");
+        } else {
+            document.body.classList.add("dark");
+        }
+
+        setDark(!dark);
+        window.localStorage.setItem("darkTheme", `${!dark}`);
+    }
+
+    return (
+        <img
+            src={dark ? Sun : Moon}
+            alt="Toggle theme"
+            width="16"
+            height="16"
+            className={styles.toggleTheme}
+            onClick={toggleTheme}
+        />
+    );
+};
 
 const Footer = () => {
     const { site } = useStaticQuery(
@@ -20,6 +65,7 @@ const Footer = () => {
         <footer className={styles.footer}>
             <hr />© {new Date().getFullYear()},{` `}
             {site.siteMetadata.author}
+            <ThemeToggle />
         </footer>
     );
 };
